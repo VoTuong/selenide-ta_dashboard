4bba3fbfbb722c6b83b4be3aaf893e02318139cb
dtuong.vo <dtuong.vo@gmail.com>
2024-10-10 22:56:35 +0700

    update isDisplayed() to shouldBe(visible)

--- src/test/java/com/tadashboard/tests/TestCaseTC001.java
+++ src/test/java/com/tadashboard/tests/TestCaseTC001.java
@@ -1,12 +1,11 @@
 package com.tadashboard.tests;
 
 
-import com.tadashboard.dataprovider.UserDataProviderFactory;
+import com.tadashboard.dataprovider.TestDataProviders;
 import com.tadashboard.enums.Repository;
 import com.tadashboard.model.user.User;
 import com.tadashboard.pages.HomePage;
 import com.tadashboard.pages.LoginPage;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -15,12 +14,13 @@ public class TestCaseTC001 extends BaseTest{
     LoginPage loginPage = new LoginPage();
     HomePage homePage = new HomePage();
 
-    @Test(retryAnalyzer = utilities.RetryAnalyzer.class, dataProvider = "validUser", dataProviderClass = UserDataProviderFactory.class)
+    @Test(retryAnalyzer = utilities.RetryAnalyzer.class,
+            dataProvider = "validUser", dataProviderClass = TestDataProviders.class)
     public void DA_LOGIN_TC001(User validUser) {
 
         loginPage.login(Repository.SAMPLE_REPOSITORY.getValue(), validUser);
 
-        Assert.assertTrue(homePage.isMainMenuDisplayed(), "Main Menu is not displayed.");
+        homePage.shouldMainMenuDisplay();
     }
 
 }
