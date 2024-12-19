17e6b00a398074674737d466d53ddaa824e39dff
dtuong.vo <dtuong.vo@gmail.com>
2024-12-19 22:14:49 +0700

    break long line

--- src/test/java/com/tadashboard/listeners/AllureListener.java
+++ src/test/java/com/tadashboard/listeners/AllureListener.java
@@ -13,49 +13,50 @@ import java.util.Objects;
 
 public class AllureListener implements TestLifecycleListener {
 
-    @Override
-    public void beforeTestSchedule(TestResult result) {
-    }
+	@Override
+	public void beforeTestSchedule(TestResult result) {
+	}
 
-    @Override
-    public void afterTestSchedule(TestResult result) {
-    }
+	@Override
+	public void afterTestSchedule(TestResult result) {
+	}
 
-    @Override
-    public void beforeTestUpdate(TestResult result) {
-    }
+	@Override
+	public void beforeTestUpdate(TestResult result) {
+	}
 
-    @Override
-    public void afterTestUpdate(TestResult result) {
-    }
+	@Override
+	public void afterTestUpdate(TestResult result) {
+	}
 
-    @Override
-    public void beforeTestStart(TestResult result) {
-    }
+	@Override
+	public void beforeTestStart(TestResult result) {
+	}
 
-    @Override
-    public void afterTestStart(TestResult result) {
-    }
+	@Override
+	public void afterTestStart(TestResult result) {
+	}
 
-    @Override
-    public void beforeTestStop(TestResult result) {
-        if (result.getStatus().equals(Status.FAILED)) {
-            if (WebDriverRunner.hasWebDriverStarted()) {
-                Allure.addAttachment(result.getName() + "_Failed_Screenshot", new ByteArrayInputStream(Objects.requireNonNull(Selenide.screenshot(OutputType.BYTES))));
-            }
-        }
-    }
+	@Override
+	public void beforeTestStop(TestResult result) {
+		if (result.getStatus().equals(Status.FAILED)) {
+			if (WebDriverRunner.hasWebDriverStarted()) {
+				Allure.addAttachment(result.getName() + "_Failed_Screenshot",
+						new ByteArrayInputStream(Objects.requireNonNull(Selenide.screenshot(OutputType.BYTES))));
+			}
+		}
+	}
 
-    @Override
-    public void afterTestStop(TestResult result) {
-    }
+	@Override
+	public void afterTestStop(TestResult result) {
+	}
 
-    @Override
-    public void beforeTestWrite(TestResult result) {
-    }
+	@Override
+	public void beforeTestWrite(TestResult result) {
+	}
 
-    @Override
-    public void afterTestWrite(TestResult result) {
-    }
+	@Override
+	public void afterTestWrite(TestResult result) {
+	}
 
 }
--- src/test/java/com/tadashboard/tests/BaseTest.java
+++ src/test/java/com/tadashboard/tests/BaseTest.java
@@ -28,60 +28,61 @@ import static com.codeborne.selenide.Selenide.screenshot;
 import static java.lang.invoke.MethodHandles.lookup;
 
 public class BaseTest {
-    private static final Logger log = LoggerFactory.getLogger(lookup().lookupClass());
-    private static final HomePage homePage = new HomePage();
+	private static final Logger log = LoggerFactory.getLogger(lookup().lookupClass());
+	private static final HomePage homePage = new HomePage();
 
-    static {
-        SelenideLogger.addListener(
-                "ReportPortal logger",
-                new ReportPortalSelenideEventListener()
-                        .enableSeleniumLogs(LogType.BROWSER, Level.FINER)
-                        .logScreenshots(true)
-                        .logPageSources(false)
-        );
-    }
+	static {
+		SelenideLogger.addListener(
+				"ReportPortal logger",
+				new ReportPortalSelenideEventListener()
+						.enableSeleniumLogs(LogType.BROWSER, Level.FINER)
+						.logScreenshots(true)
+						.logPageSources(false)
+		);
+	}
 
-    @Parameters({"browser", "runMode"})
-    @BeforeClass
-    public void setUp(String browser, String runMode) {
-        if ("grid".equalsIgnoreCase(runMode)) {
-            Configuration.remote = "http://localhost:4444/";
-        }
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                Configuration.browser = "chrome";
-                break;
-            case "edge":
-                Configuration.browser = "edge";
-                break;
-            default:
-                throw new IllegalArgumentException("Browser not supported: " + browser);
-        }
-        Configuration.headless = false;
-        log.info("Start {} TestNG tests in {}", getClass().getName(), Configuration.browser);
-    }
+	@Parameters({"browser", "runMode"})
+	@BeforeClass
+	public void setUp(String browser, String runMode) {
+		if ("grid".equalsIgnoreCase(runMode)) {
+			Configuration.remote = "http://localhost:4444/";
+		}
+		switch (browser.toLowerCase()) {
+			case "chrome":
+				Configuration.browser = "chrome";
+				break;
+			case "edge":
+				Configuration.browser = "edge";
+				break;
+			default:
+				throw new IllegalArgumentException("Browser not supported: " + browser);
+		}
+		Configuration.headless = false;
+		log.info("Start {} TestNG tests in {}", getClass().getName(), Configuration.browser);
+	}
 
-    @BeforeMethod
-    public void launch() {
-        open("http://localhost/TADashboard/login.jsp");
-        WebDriverRunner.getWebDriver().manage().window().maximize();
-    }
+	@BeforeMethod
+	public void launch() {
+		open("http://localhost/TADashboard/login.jsp");
+		WebDriverRunner.getWebDriver().manage().window().maximize();
+	}
 
-    @AfterMethod
-    public void tearDown() {
-        homePage.logout();
-        log.info("Finished {} TestNG tests in {}", getClass().getName(), Configuration.browser);
-        Selenide.closeWebDriver();
-    }
+	@AfterMethod
+	public void tearDown() {
+		homePage.logout();
+		log.info("Finished {} TestNG tests in {}", getClass().getName(), Configuration.browser);
+		Selenide.closeWebDriver();
+	}
 
-    @AfterMethod
-    public void addAttachmentOnFailure(ITestResult testResult){
-        if (!testResult.isSuccess()) {
-            if (WebDriverRunner.getWebDriver() instanceof TakesScreenshot) {
-                File screenshot = screenshot(OutputType.FILE);
-                ReportPortal.emitLog("Test failed - Screenshot attached " + testResult.getMethod().getMethodName(), "ERROR", new Date(), screenshot);
-            }
-        }
-    }
+	@AfterMethod
+	public void addAttachmentOnFailure(ITestResult testResult) {
+		if (!testResult.isSuccess()) {
+			if (WebDriverRunner.getWebDriver() instanceof TakesScreenshot) {
+				File screenshot = screenshot(OutputType.FILE);
+				ReportPortal.emitLog("Test failed - Screenshot attached " + testResult.getMethod().getMethodName(),
+						"ERROR", new Date(), screenshot);
+			}
+		}
+	}
 
 }
